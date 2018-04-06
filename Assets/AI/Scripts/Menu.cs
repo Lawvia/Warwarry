d7a5cbf57daf338e984d4dd88a4e75c8f4a3d0b1
lawvia <rsndvdsn@gmail.com>
2018-04-06 17:07:22 +0700

    Added newest update , Demo playable now

---
+++ Assets/AI/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	private bool canContinue = true;
+
+	void Start () {
+		//string checkLevelName = PlayerPrefs.GetString("savedLevel");
+	//	if(checkLevelName == null || checkLevelName == ""){
+	//		canContinue = false;
+	//	}
+	}
+
+	void newGame () {
+		//PlayerPrefs.DeleteAll();
+		Application.LoadLevel("vsAI-1");
+	}
+
+	void loadGame () {
+		//if (canContinue) {
+	//		string levelName = PlayerPrefs.GetString ("savedLevel");
+	//		Application.LoadLevel (levelName);
+		//}
+		Application.LoadLevel("starting");
+	}
+}
---
+++ Assets/Tanks/ScriptPrefabAI/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Tembakan : MonoBehaviour
+{
+	public LayerMask m_TankMask;
+	public ParticleSystem m_ExplosionParticles;       
+	public AudioSource m_ExplosionAudio;              
+	public float m_MaxDamage = 100f;                  
+	public float m_ExplosionForce = 1000f;            
+	public float m_MaxLifeTime = 2f;                  
+	public float m_ExplosionRadius = 5f;              
+
+
+	private void Start()
+	{
+		Destroy(gameObject, m_MaxLifeTime);
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		Collider[] colliders = Physics.OverlapSphere(transform.position,m_ExplosionRadius,m_TankMask);
+
+		for(int i=0; i < colliders.Length; i++){
+			Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody> ();
+
+			if (!targetRigidbody)
+				continue;
+				
+			targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+			PlayerHealth targetHealth = targetRigidbody.GetComponent<PlayerHealth> ();
+
+			if (!targetHealth)
+				continue;
+
+			float damage = CalculateDamage (targetRigidbody.position);
+
+			targetHealth.TakeDamage (damage);
+			
+		}
+
+		m_ExplosionParticles.transform.parent = null;
+		m_ExplosionParticles.Play ();
+		m_ExplosionAudio.Play ();
+
+		ParticleSystem.MainModule mainModule = m_ExplosionParticles.main;
+		Destroy (m_ExplosionParticles.gameObject, mainModule.duration);
+		Destroy (gameObject);
+	}
+
+
+	private float CalculateDamage(Vector3 targetPosition)
+	{
+		
+		Vector3 explosionToTarget = targetPosition - transform.position;
+
+		float explosionDistance = explosionToTarget.magnitude;
+		float RelativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+		float damage = RelativeDistance * m_MaxDamage;
+		damage = Mathf.Max (0f, damage);
+
+		return damage;
+	}
+}
